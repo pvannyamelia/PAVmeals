ee219d50ea1a31aa468baa62f642c8751a7789d6
pvannyamelia <50089393+pvannyamelia@users.noreply.github.com>
2020-12-10 08:34:28 +0700

    Pisah folder, Android dan API

--- app/src/main/java/com/ciputra/pavmeals/android/MainActivity.kt
+++ app/src/main/java/com/ciputra/pavmeals/android/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ciputra.pavmeals
+package com.ciputra.pavmeals.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ciputra.pavmeals.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/ciputra/pavmeals/api/ApiService.kt
@@ -0,0 +1,5 @@
+package com.ciputra.pavmeals.api
+
+interface ApiService {
+
+}
