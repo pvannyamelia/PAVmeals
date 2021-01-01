42a9bf5e992b73fb16fd1fddf688e57c79684c90
pvannyamelia <50089393+pvannyamelia@users.noreply.github.com>
2021-01-01 22:25:25 +0700

    coba recyclerview

--- app/src/main/java/com/ciputra/pavmeals/android/MainActivity.kt
+++ app/src/main/java/com/ciputra/pavmeals/android/MainActivity.kt
@@ -3,10 +3,19 @@ package com.ciputra.pavmeals.android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ciputra.pavmeals.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rv_area.setHasFixedSize(true)
+        rv_category.setHasFixedSize(true)
+        rv_ingredient.setHasFixedSize(true)
+    }
+
+    private fun showCategoryRecycler(){
+
     }
 }
