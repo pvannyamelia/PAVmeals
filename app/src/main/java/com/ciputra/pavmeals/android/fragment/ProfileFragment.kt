46776f414ab402da9571784897c1643b99ac219b
pvannyamelia <50089393+pvannyamelia@users.noreply.github.com>
2021-01-21 08:55:41 +0700

    Update ProfileFragment.kt

--- app/src/main/java/com/ciputra/pavmeals/android/fragment/ProfileFragment.kt
+++ app/src/main/java/com/ciputra/pavmeals/android/fragment/ProfileFragment.kt
@@ -65,7 +65,6 @@ class ProfileFragment : Fragment() {
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
             this.requireActivity().supportFragmentManager.popBackStack()
             startActivity(intent)
-            this.requireActivity().supportFragmentManager.popBackStack()
 
         }
     }
